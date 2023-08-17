3ae5333042172881f2ee8f5197eb2fc17c01fa44
Farzana Yesmin <yesminfarzana.cse@gmail.com>
2023-08-17 17:13:31 +0600

    Topbar and Navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Topbar from './components/Topbar';
+import Navbar from './components/Navbar';
+
+function App() {
+  
+
+  return (
+    <>
+      <Topbar />
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
