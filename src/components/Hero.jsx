ba04d40d60e68b1921acd88904a8b526f038b314
Farzana Yesmin <yesminfarzana.cse@gmail.com>
2023-08-22 15:30:04 +0600

    2 pages Created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         />
 
         <div className='max-w-[1140px] m-auto'>
-            <div className='absolute top-[40%] w-full md:-[50%] max-w-[600px] h-full flex flex-col text-slate-800 p-4'>
+            <div className='absolute top-[40%] w-full md:-[50%] max-w-[600px] h-full flex flex-col text-black p-4'>
                 <h1 className='font-bold text-4xl '>Find Your Special Trip</h1>
                 <h2 className='text-3xl py-4 italic'>With Weekaway</h2>
                 <p>
